3303688f68a989b05d2b303aab7f7c7ba6a3d2b9
moreasim <moreasim@pc-dg-035-05.iut2.upmf-grenoble.fr>
2018-05-29 08:26:35 +0200

    version1

---
+++ Monopoly/src/monopoly/Cellule.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monopoly;
+
+/**
+ *
+ * @author fourrieq
+ */
+public class Cellule {
+    private String nom;
+    private int numero;
+    private Plateau plateau;
+
+    public Cellule(String nom, int numero) {
+        this.nom = nom;
+        this.numero = numero;
+    }
+    
+    public String getNomCase(int numero){
+        // A FAIRE
+    }
+    public Joueur getProprietaire(){
+        // A FAIRE
+    }
+    public void removePion(Cellule cAvant){
+        
+    }
+    public void addPions(Joueur pion){
+        
+    }
+    public void test(){
+        
+    }
+}
