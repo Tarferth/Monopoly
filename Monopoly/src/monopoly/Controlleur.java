d9d1624ea7458785a5aabc95f30a133086fce099
moreasim <moreasim@pc-dg-035-05.iut2.upmf-grenoble.fr>
2018-05-29 09:15:10 +0200

    inscription joueur partie 1

--- Monopoly/src/monopoly/Controlleur.java
+++ Monopoly/src/monopoly/Controlleur.java
@@ -6,6 +6,7 @@
 package monopoly;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -13,14 +14,33 @@ import java.util.ArrayList;
  */
 public class Controlleur {
     private ArrayList<Joueur> joueurs;
+    private boolean faitUnDouble;
+    
     public int lancerDes(){
-        // A FAIRE
+        int lancer1 = (int) Math.round(Math.random()*5+1);
+        int lancer2 = (int) Math.round(Math.random()*5+1);
+        if (lancer1 == lancer2){
+            faitUnDouble = true;
+        }
+        else
+            faitUnDouble = false;
+        
+       return lancer1+lancer2;
     }
     public Joueur getJoueurCourant(){
-        // A FAIRE
+       
     }
     public int getPrixLot(int l){
         // A FAIRE
+    }
+    
+    public Joueur nouveauJoueur(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choisissez votre nom : ");
+        String nom = sc.nextLine();
+        System.out.println("Choisissez votre symbole : ");
+        String symbole = sc.nextLine();
+        return new Joueur(nom,symbole);
     }
    
 }
