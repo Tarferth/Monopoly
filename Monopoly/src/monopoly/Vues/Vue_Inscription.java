ebc0881c56acd0ad92f8a90ed70a3829044d28d0
fourrieq <fourrieq@PC-DG-021-16.ntiut2gre.iut2.upmf-grenoble.fr>
2018-06-18 11:40:19 +0200

    cheval numéro 10

--- Monopoly/src/monopoly/Vues/Vue_Inscription.java
+++ Monopoly/src/monopoly/Vues/Vue_Inscription.java
@@ -5,10 +5,31 @@
  */
 package monopoly.Vues;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
 /**
  *
  * @author fourrieq
  */
 public class Vue_Inscription {
+    public Vue_Inscription(){
+        private JFrame f_Inscription;
+        
+        f_Inscription = new JFrame();
+        f_Inscription.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        f_Inscription.setSize(dim);
+        
+    }
     
+    public void afficher() {
+        this.f_Inscription.setVisible(true);
+    }
+
+    public static void main(String [] args) {
+        Vue_Inscription exemple1 = new Vue_Inscription();
+        exemple1.afficher();
+   }  
 }
