d1c9fec2c15d877100755bcdc0283843b7510612
crosv <crosv@PC-DG-021-17.ntiut2gre.iut2.upmf-grenoble.fr>
2018-06-21 15:58:53 +0200

    debut vue jeu

--- Monopoly/src/monopoly/Vue_Jeu.java
+++ Monopoly/src/monopoly/Vue_Jeu.java
@@ -5,10 +5,22 @@
  */
 package monopoly;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
 /**
  *
  * @author crosv
  */
 public class Vue_Jeu {
+    JFrame f_Jeu;
     
+    public Vue_Jeu()
+    {
+        f_Jeu = new JFrame();
+        f_Jeu.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        
+    }
 }
